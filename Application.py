e11ce4b7d122d6ec4eb352149916d13b08bbbd4c
Zheak <milnejacob@hotmail.com>
2021-05-09 15:32:47 -0600

    Added methods to Actively Clean and visualize Dividend Data

--- Application.py
+++ Application.py
@@ -1,5 +1,3 @@
-
-
 import pandas as pd
 import streamlit as sl
 import yfinance as yf
@@ -12,37 +10,42 @@ These are stock closing price and volume of S&P 500 Stocks and Google
 """)
 #Define ticker symbol and get data on the ticker
 
-
-def write_charts(inlist):
+#Creates graphics for Charts passed into method
+def write_charts(inlist,tickers):
+    incrementer = 0
     for i in inlist:
-        sl.write(""" 
-                 # Closing Price """)
-        sl.line_chart(i.Close)
-        
-        sl.write("""
-                 # Volume
-                 """)
+        print(type(i))
+        sl.write("""# Closing Price """ + tickers[incrementer])
+        sl.line_chart(i.Close)       
+        sl.write("""# Volume """ + tickers[incrementer])
         sl.line_chart(i.Volume)
-    
+        if sum(i.Dividends) != 0:
+            dividends = get_dividends(i.Dividends)
+            sl.write("""# Dividends """ + tickers[incrementer])
+            sl.line_chart(dividends)
+            sl.write(dividends)
+            sl.write(type(dividends))
+        incrementer +=1       
+# Gets Data for list of tickers sent into the method
 def get_data(inlist):
     outlist =[]
     for i in inlist:
         outlist.append(yf.Ticker(i))
     return outlist
-
+#Returns Datafiles for history for passed in list
 def get_df(data):
     outlist = []
     for i in data:
         outlist.append(i.history(period='1d', start = '2010-5-31',end ='2020-5-31'))
     return outlist
 
+def get_dividends(stock):
+    outlist = stock[stock != 0]
+    return outlist
+              #  stock.Dividends.pop(x)
 
-tickerSymbols = ['GOOGL', 'MMM', 'ABT', 'ABBV']
+tickerSymbols = ['BTC-USD','ETH-USD', 'DOGE-USD', 'GOOGL', 'MMM', 'ABT', 'ABBV']
 tickerData = get_data(tickerSymbols)
-
-type(tickerData)
-
-sl.write(type(tickerData))
 tickerDf = get_df(tickerData)
 
-write_charts(tickerDf)
+write_charts(tickerDf,tickerSymbols)
